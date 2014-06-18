f363684dd800e2e30c704bbef0499fc655b45d41
Binh Nguyen Thai <posseidon@gmail.com>
2014-06-18 17:52:33 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hmage::Application.config.secret_token = '68237a865bd2cc2c31a1a1fdfde75c2ac2316cc8fdfc022a4710e5134dbb131acb839286240f6f1fbcc0f7febfd212b1747e97a8e1701a05d88c224bebb73201'
