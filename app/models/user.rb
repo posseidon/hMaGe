a7d9ec4a02e273c34612c7aecb7b21f1db9a93ea
Binh Nguyen Thai <posseidon@gmail.com>
2014-08-09 21:10:24 +0200

    Resolved #29

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable
   validates_uniqueness_of :email
+  validates :name, :email, :password, presence: true
+  validates :password, :presence =>true, :confirmation => true
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :role
