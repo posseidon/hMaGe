c843a72664b08cc3089b5af4542c80f34a28f10a
Binh Nguyen Thai <posseidon@gmail.com>
2015-08-19 17:26:22 +0200

    Enhanced map#edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def user_role_to_icon
+        if current_user.role.eql? 'admin'
+        	"<i class='glyphicon glyphicon-tower icon'></i> &nbsp;".html_safe
+        elsif current_user.role.eql? 'viewer'
+        	"<i class='fa fa-user icon'></i> &nbsp;".html_safe
+        elsif current_user.role.eql? 'editor'
+        	"<i class='fa fa-users icon'></i> &nbsp;".html_safe
+        else
+        	"<i class='fa fa-university icon'></i> &nbsp;".html_safe
+        end
+	end
 end
